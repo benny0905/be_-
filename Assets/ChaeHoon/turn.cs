bb97b30a1ea36dbd04a97ef9a04240c24a6a7600
CHEK <chekong@naver.com>
2017-11-04 17:19:44 +0900

    CHEK

---
+++ Assets/ChaeHoon/turn.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class turn : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnMouseDown()
+    {
+        for (int n = 0; n < 60; n++)
+        {
+            transform.Rotate(0, 0, 1);
+            for(int time = 0; time < 1000; time++)
+            {
+
+            }
+        }
+    }
+}
