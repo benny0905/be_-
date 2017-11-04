a79467d2188cd4b4e2175b6d8a4e90b00f8fe888
Orchid <orchidbird2@gmail.com>
2017-11-04 16:06:25 +0900

    회전 및 연결위치 정보 저장

---
+++ Assets/NanSae/Block.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Block : MonoBehaviour{
+	public int positionX;
+	public int positionY;
+	public int positionZ;
+	public List<bool> pipeDirection = new List<bool>();
+	void OnMouseDown(){
+		transform.Rotate(0, 0, 60);
+
+		//새로운 연결방향 계산
+		List<bool> newDirections = new List<bool>();
+		newDirections.Add(pipeDirection[5]);
+		for(int i = 0; i < 5; i++){
+			newDirections.Add(pipeDirection[i]);
+		}
+
+		pipeDirection = newDirections;
+	}
+
+	void Start(){
+		/*for(int i = 0; i < 6; i++){
+			pipeDirection.Add(false);
+		}
+		pipeDirection[0] = true;
+		pipeDirection[3] = true;*/
+		transform.position = worldPosition;
+	}
+
+	public Vector3 worldPosition{
+		get{
+			return new Vector3((positionX-positionY)*1.5f, (positionX+positionY)*2.4f, 0);
+		}
+	}
+
+	/*int nextDirection(int currentDirection){
+		if(currentDirection == 5){
+			return 0;
+		}else{
+			return currentDirection+1;
+		}
+	}*/
+}
