4a20d7dd7ed7be2956086c88c80c23eb42ad620c
CHEK <chekong@naver.com>
2017-11-11 14:20:18 +0900

    .

---
+++ Assets/ChaeHoon/Sources/rotate.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotate : MonoBehaviour
+{
+    IEnumerator SmoothRotation()
+    {
+        for (int num = 0; num < 30; num++)
+        {
+            yield return new WaitForSeconds(QuadraticFormula(num));
+            transform.Rotate(0, 0, 2);
+        }
+    }
+    private float QuadraticFormula(float x)
+    {
+        float y;
+        y = 0.000215f * (x - 15.0f) * (x - 15.0f) + 0.0001f;
+        return y;
+    }
+    private void OnMouseDown()
+    {
+        StartCoroutine("SmoothRotation");
+    }
+}
