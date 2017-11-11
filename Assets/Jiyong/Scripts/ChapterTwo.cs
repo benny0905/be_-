617646ff168a3ef07ad8d0d3a066ebdad8a84f78
jy02sung <jy02sung@naver.com>
2017-11-11 12:58:58 +0900

    merge

---
+++ Assets/Jiyong/Scripts/ChapterTwo.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChapterTwo : MonoBehaviour
+{
+
+    public void Button()
+    {
+        Invoke("chapterTwo", 0.3f);
+    }
+    private void chapterTwo()
+    {
+        SceneManager.LoadScene("Chap 2");
+    }
+}
